a0491c65890a21f3ff1ea592a0cc50328c1aa575
anushhka sharma <177105799+AnushkaTechFlux@users.noreply.github.com>
2025-09-01 20:26:36 +0530

    📅 Day 5 of 100 – Primitive Data Types
    📅 Day 5 of 100 – Primitive Data Types
    
    🔹 Goal: Understand and practice using primitive data types in Java.
    🔹 Why this task?
    Primitive data types are the building blocks of variables. Without them, you can’t store or work with data in your program.

---
+++ primitiveDataTypes.java
@@ -0,0 +1,20 @@
+package javaPrograms;
+public class primitiveDataTypes {
+    public static void main(String[] args) {
+        byte age = 25;
+        short salary = 10000;
+        long distance = 273767382989L;
+        float weight = 55.5f;
+        double pi = 3.14563728938;
+        char grade = 'A';
+        boolean javaIsFun = true;
+
+        System.out.println("Byte : "+age);
+        System.out.println("Short : "+salary);
+        System.out.println("Long: "+ distance);
+        System.out.println("Float: "+weight);
+
+    }
+}
+    
+
