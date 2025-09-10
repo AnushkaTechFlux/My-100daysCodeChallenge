610e3ecc6e91136d3b29348ebd5a3944a43a464b
anushhka sharma <177105799+AnushkaTechFlux@users.noreply.github.com>
2025-09-10 12:56:17 +0530

    📅 Day 12 of 100 – Pyramid Pattern
    📅 Day 12 of 100 – Pyramid Pattern
    
    🔹 Goal: Print a pyramid pattern of stars using loops.
    🔹 Why this task?
    It helps practice nested loops, spacing, and pattern logic, which is crucial for visualizing problems in DSA.

---
+++ Day-12.java
@@ -0,0 +1,18 @@
+package javaPrograms;
+    public class patterPrint3{
+        public static void main(String[]args){
+            int n = 5 ;
+            int space = n-1 ;
+            int star = 1 ;
+            for (int i = 1 ; i<=n ; i++){
+                for (int j = 1 ; j<=space ; j++){
+                    System.out.println();
+                }
+                space--;
+                star++;
+                System.out.println();
+            }
+        
+    }
+}
+
